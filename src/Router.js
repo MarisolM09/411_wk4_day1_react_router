9eb959fc2197341d95b6d0ef71c15bac8c2dda4d
MarisolM09 <mmendez9980@gmail.com>
2022-08-22 20:47:21 -0500

    added css to material ui components

--- src/Router.js
+++ src/Router.js
@@ -8,10 +8,22 @@ to the About component.
 Remember to export the component at the end
 */
 
-import React from 'react'
-import { Routes, Route } from 'react-router'
-// Write component imports here //
+import React from "react";
+import { Routes, Route } from "react-router";
+import Home from "./components/Home";
+import About from "./components/About";
+import Car from "./components/Car"
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/car/:id" element={<Car />}></Route>
+    </Routes>
+  );
+};
+
+export default Router;
 
 
-
-// Start Router function here //
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import cars from '../cars.json'
-// Import {useParams} from "react-router-dom" here //
-
-// import MUI components here //
-// Container, Paper, Chip //
+import React from "react";
+import cars from "../cars.json";
+import { useParams } from "react-router-dom";
+import { Container, Chip, Paper} from "@mui/material";
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
-}
+  const { id } = useParams();
+  let car = cars.find((car) => car.id == id);
 
-export default Car
+  return (
+    <Container classsName="container" maxWidth="sm">
+      
+        <Paper elevation={3}>
+          <h2>{car.Name}</h2>
+          <div className="chip">
+            <Chip classname="chip" label={`Id: ${car.id}`} />
+            <Chip label={`Miles Per Gallon: ${car.Miles_per_Gallon}`} />
+            <Chip label={`Cylinders: ${car.Displacement}`} />
+            <Chip label={`Displacement: ${car.Horsepower}`} />
+            <Chip label={`Weight: ${car.Weight_in_lbs} lbs`} />
+            <Chip label={`Acceleration: ${car.Acceleration}`} />
+            <Chip label={`Year: ${car.Year}`} />
+            <Chip label={`Origin: ${car.Origin}`} />
+          </div>
+        </Paper>
+    
+    </Container>
+  );
+};
+
+export default Car;
